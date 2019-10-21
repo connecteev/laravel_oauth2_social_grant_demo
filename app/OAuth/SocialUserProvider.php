5aab828a6c71540d7a43600244db2566e7fa4e20
Kunal Punjabi <kunalspunjabi@gmail.com>
2019-10-21 12:57:40 -0700

    Create Social User from Socialite Data, remove redundant app/Repositories directory

--- app/OAuth/SocialUserProvider.php
+++ app/OAuth/SocialUserProvider.php
@@ -2,38 +2,26 @@
 
 namespace App\OAuth;
 
-use App\Repositories\UserRepository;
 use Laravel\Socialite\Facades\Socialite;
 use Laravel\Passport\Bridge\User as UserEntity;
 use League\OAuth2\Server\Exception\OAuthServerException;
+use App\User;
 
 class SocialUserProvider implements SocialUserProviderInterface
 {
-    /**
-     * The user repository instance.
-     * 
-     * @var UserRepository
-    */
-    protected $userRepository;
-
-    /**
-     * Create a social user provider instance.
-     * 
-     * @param UserRepository  $userRepository
-    */
-    public function __construct(UserRepository $userRepository)
+    public function __construct()
     {
-        $this->userRepository = $userRepository;
+        //
     }
 
     /**
      * {@inheritdoc}
-    */
+     */
     public function getUserEntityByAccessToken($provider, $accessToken)
     {
         $user = $this->getUserFromSocialProvider($provider, $accessToken);
 
-        if (! $user) {
+        if (!$user) {
             return;
         }
 
@@ -42,14 +30,14 @@ class SocialUserProvider implements SocialUserProviderInterface
 
     /**
      * Get the user from the specified provider using the given access token.
-     * 
+     *
      * @param string  $provider
      * @param string  $accessToken
-     * 
+     *
      * @throws \League\OAuth2\Server\Exception\OAuthServerException
-     * 
+     *
      * @return \App\User
-    */
+     */
     public function getUserFromSocialProvider($provider, $accessToken)
     {
         try {
@@ -59,11 +47,30 @@ class SocialUserProvider implements SocialUserProviderInterface
                 'Authentication error, invalid access token',
                 $errorCode = 400,
                 'invalid_request'
-            );            
+            );
         }
 
-        return $this->userRepository->findOrCreateSocialUser(
-            array_merge($user->getRaw(), ['provider' => $provider])
+        return $this->findOrCreateSocialUser(
+            $user,
+            $provider,
+        );
+    }
+
+    private function findOrCreateSocialUser($socialiteUser, $provider)
+    {
+        return User::firstOrCreate(
+            [
+                'email'             => $socialiteUser->getEmail(),
+            ],
+            [
+                'name'              => $socialiteUser->getName(),
+                'email'             => $socialiteUser->getEmail(),
+                'social_id'         => $socialiteUser->getId(),
+                'service'           => $provider,
+                'social_name'       => $socialiteUser->getName(),
+                'social_username'   => $socialiteUser->getNickname(),
+                'social_avatar'     => $socialiteUser->getAvatar(),
+            ],
         );
     }
 }
